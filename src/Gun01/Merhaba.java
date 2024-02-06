a4d4684573a5037ef61f84cfacccbbf36509c4ad
beyzanurer <beyzanur.er@outlook.com>
2024-02-07 01:35:56 +0300

    Satın alma işlemi onayı eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,6 +4,9 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
+
+        for (int i=0; i<5 ; i++){
+            System.out.println("merhaba");
+        }
     }
 }
