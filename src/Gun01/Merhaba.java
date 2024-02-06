4369d79d6bb2ae68f3d38647f531432a6dee3115
beyzanurer <beyzanur.er@outlook.com>
2024-02-07 01:33:29 +0300

    Kredi kartı kontrolü eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,5 +4,6 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
+        System.out.println("Merhaba 3");
     }
 }
